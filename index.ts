40655e2ba181d419b035c49d4516f5e8a73ffdbd
subhoS <subhadeepdatta2@gmail.com>
2021-07-14 00:06:30 +0530

    minor chnages

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("hemlo");
+let fastify = require('fastify')({
+  logger: true
+})
+
+const app = fastify;
+
+app.get('/', async (request : string, reply : string) => {
+  return 'working'
+})
+
+app.listen(3000, (err : string, address : string) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
